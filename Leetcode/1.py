bdbe4f4acf01ac51b32c443fca5cd897fc0356d4
sannisthsoni <soni.sannisth@gmail.com>
2018-05-31 00:00:20 -0700

    Reverse Function added.

---
+++ Leetcode/1.py
@@ -0,0 +1,25 @@
+# Given a binary matrix A, we want to flip the image horizontally, 
+# then invert it, and return the resulting image.
+
+# To flip an image horizontally means that each row of the image is reversed.  
+# For example, flipping [1, 1, 0] horizontally results in [0, 1, 1].
+
+# To invert an image means that each 0 is replaced by 1, and each 1 is replaced by 0. 
+# For example, inverting [0, 1, 1] results in [1, 0, 0].
+
+
+def reverse(matrix):
+    for matrix_col in matrix:
+        length = len(matrix_col)
+        #print(int((length/2)))
+        for i in range(int(len(matrix_col)/2)):
+            temp=matrix_col[i]
+            matrix_col[i] = matrix_col[len(matrix_col)-i-1]
+            matrix_col[len(matrix_col)-i-1] = temp
+        
+    return matrix
+
+print(reverse([[1,1,0],[0,0,1]]))
+
+
+
