8cbe495f28bc28a7b0abcfc202cdca1ce4dd0d61
sannisthsoni <soni.sannisth@gmail.com>
2018-05-31 04:24:57 -0700

    Solution from leetcode added

--- Leetcode/ReshapeMatrix.py
+++ Leetcode/ReshapeMatrix.py
@@ -12,23 +12,37 @@
 #If the 'reshape' operation with given parameters is possible and legal, 
 # output the new reshaped matrix; Otherwise, output the original matrix. 
 
-#Solution v1.0
-def MatrixToList(matrix):
-    l = []
-    for row in matrix:
-        for element in row:
-            l.append(element)
-    return l
+#Solution v1.1 (From Leetcode comment by 'liaowy') https://leetcode.com/articles/reshape-the-matrix/#
+def matrixReshape(self, nums, r, c):
+    matrix=[]
+    one_row=[]
+    for i in nums:
+        for j in i:
+            one_row.append(j)
+    if r*c == len(one_row):
+        for i in range(0,len(one_row),c):
+            matrix.append(one_row[i:i+c])
+        return matrix
+    else:
+        return nums
 
-def ListToMatrix(l,r,c):
-    length = len(l)
-    matrix = []
-    temp=[]
-    for i in range(length):
-        temp.append(l[i])
-        if (i+1)%c == 0:
-            matrix.append(temp)
-            temp = []
-    return matrix
-#print(MatrixToList([[1,2],[3,4]]))
-print(ListToMatrix([1,2,3,4,5,6,7,8,9,10],5,2))
+#Solution v1.0
+# def MatrixToList(matrix):
+#     l = []
+#     for row in matrix:
+#         for element in row:
+#             l.append(element)
+#     return l
+
+# def ListToMatrix(l,r,c):
+#     length = len(l)
+#     matrix = []
+#     temp=[]
+#     for i in range(length):
+#         temp.append(l[i])
+#         if (i+1)%c == 0:
+#             matrix.append(temp)
+#             temp = []
+#     return matrix
+# #print(MatrixToList([[1,2],[3,4]]))
+# print(ListToMatrix([1,2,3,4,5,6,7,8,9,10],5,2))
