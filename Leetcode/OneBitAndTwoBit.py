19ff670f48ade66a89af463696f899865f93b87e
sannisthsoni <soni.sannisth@gmail.com>
2018-07-04 01:42:33 -0700

    OneBitAndTwiBit.py added

---
+++ Leetcode/OneBitAndTwoBit.py
@@ -0,0 +1,13 @@
+#We have two special characters. The first character can be represented by one bit 0. 
+#The second character can be represented by two bits (10 or 11).
+#Now given a string represented by several bits. Return whether the last character must be a one-bit character or not. The given string will always end with a zero.
+
+def isOneBitCharacter(bits):
+    length = len(bits)
+    bits = bits[:-1]
+
+    
+
+    print(bits)
+
+print(isOneBitCharacter( [1,0,0]))
