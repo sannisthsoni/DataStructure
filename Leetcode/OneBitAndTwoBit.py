d58a8c1c85351fe45cab21175daa09dd9500df81
sannisthsoni <soni.sannisth@gmail.com>
2018-07-07 18:07:29 -0700

    Solution added

--- Leetcode/OneBitAndTwoBit.py
+++ Leetcode/OneBitAndTwoBit.py
@@ -2,12 +2,13 @@
 #The second character can be represented by two bits (10 or 11).
 #Now given a string represented by several bits. Return whether the last character must be a one-bit character or not. The given string will always end with a zero.
 
+
+#SOlution from solutions/leetcode
 def isOneBitCharacter(bits):
-    length = len(bits)
-    bits = bits[:-1]
+    i = 0
 
-    
-
-    print(bits)
+    while i< len(bits):
+        i += bits[i] + 1
+    return i == len(bits)-1
 
 print(isOneBitCharacter( [1,0,0]))
